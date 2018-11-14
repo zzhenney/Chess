1983008bf40849c75ec1a39b430276b538ddfe4f
Julian <teritry2@gmail.com>
2018-11-13 20:18:56 -0800

    Minor cleanup

--- chess-game/board_interaction.js
+++ chess-game/board_interaction.js
@@ -3,6 +3,9 @@ module.exports = {
 
     },
     tileIsEmpty(cordX, cordY){
+        if(this.isOutOfBounds(cordX, cordY)){
+            return false
+        }
         return true
     },
     piece_is_enemy_passant_pawn: function(playerColor, cordX, cordY){
@@ -10,7 +13,7 @@ module.exports = {
         // Implement Check if enemy pawn at tile which moved two fields last turn. If yes return true.
         return true 
     },
-        isOutOfBounds(cordX,cordY){
+    isOutOfBounds(cordX,cordY){
         return(cordX >= 0 && cordX < 8 && cordY >= 0 && cordY < 8)
     }
     
