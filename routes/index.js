dc7bdcf8270d7d8ca907118d303f6759f63f307c
Zac Henney <zachenney@Zacs-MacBook-Pro-2.local>
2018-12-20 20:46:17 -0800

    Fixing quit game

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const isAuthenticated = require('../auth/isAuthenticated');
 
 router.get('/', isAuthenticated, (_, response) => {
-  response.render('index', { id: 0 });
+  response.render('index');
 });
 
 module.exports = router;
